8e2118d4427734115db34752b1c267b619c50ba8
Randika Chandrapala <randikac32@gmail.com>
2019-05-22 00:17:30 +0530

    * Initial state of Settings VC done. * Need to implement preferences.

--- TweetsFelt/Controllers/SettingsViewController.swift
+++ TweetsFelt/Controllers/SettingsViewController.swift
@@ -10,20 +10,35 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    
+    @IBOutlet weak var tweetsCountLbl: UILabel!
     @IBOutlet weak var avoidRetweetsSwitch: UISwitch!
-    @IBOutlet weak var avoidReplyTweets: UISwitch!
+    @IBOutlet weak var avoidReplyTweetsSwitch: UISwitch!
+    
+    // Should come from App Preferences
+    var tweetsCount: Int = 20
+    var avoidRetweets: Bool = false
+    var avoidReplyTweets: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tweetsCountLbl.text = "20"
+        avoidRetweetsSwitch.setOn(false, animated: false)
+        avoidReplyTweetsSwitch.setOn(false, animated: false)
+    }
+    
+    @IBAction func tweetsCountStepperValueChanged(_ sender: UIStepper) {
+        tweetsCount = Int(sender.value)
+        self.tweetsCountLbl.text = String(tweetsCount)
     }
     
     @IBAction func avoidRetweetsSwitchValueChanged(_ sender: UISwitch) {
+        avoidRetweets = sender.isOn
     }
     
     @IBAction func avoidReplyTweetsValueChanged(_ sender: UISwitch) {
+        avoidReplyTweets = sender.isOn
     }
     
     @IBAction func saveBtnClicked(_ sender: UIButton) {
