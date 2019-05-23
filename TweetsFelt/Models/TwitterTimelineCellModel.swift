bad8fdc831b30766dcfd9154f56283245e076b89
Randika Chandrapala <randikac32@gmail.com>
2019-05-23 08:59:06 +0530

    * Initiating MVVM implementation

---
+++ TweetsFelt/Models/TwitterTimelineCellModel.swift
@@ -0,0 +1,9 @@
+//
+//  TwitterTimelineCellModel.swift
+//  TweetsFelt
+//
+//  Created by Randika Vishman on 5/23/19.
+//  Copyright © 2019 randika. All rights reserved.
+//
+
+import Foundation
---
+++ TweetsFelt/Models/TwitterTimelineVCModel.swift
@@ -0,0 +1,9 @@
+//
+//  TwitterTimelineVCModel.swift
+//  TweetsFelt
+//
+//  Created by Randika Vishman on 5/23/19.
+//  Copyright © 2019 randika. All rights reserved.
+//
+
+import Foundation
